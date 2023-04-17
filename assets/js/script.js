c9c5dd45adaddfabed477287bfb184b80e6131ec
Marq W <marquisewest86@duck.com>
2023-04-17 01:55:54 -0500

    got weather search functioning at a rudimentary level

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,38 @@
-var APIkey = "e8835723679dd5fa30210dbd83917a83";
+$(document).ready(function () {
+  var APIkey = "e8835723679dd5fa30210dbd83917a83";
 
-var city = "austin"
-var queryURL ="api.openweathermap.org/data/2.5/weather?q=austin&appid=e8835723679dd5fa30210dbd83917a83"
+  var searchButton = "hello";
 
-var queryURL2 = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e8835723679dd5fa30210dbd83917a83"
-var showFetch = fetch(queryURL2);
-console.log(showFetch);
+  $(".btn").on("click", function (event) {
+    event.preventDefault();
+    //   console.log("Form submitted");
+    var city = $("#chosen-city").val();
+    var queryURL =
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      APIkey;
 
+    var queryURL2 =
+      "http://api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
+      "&appid=" +
+      APIkey;
+
+    fetch(queryURL)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+
+    fetch(queryURL2)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  });
+});
