90956535d2fb3f4fbcca00d3d0ce5656f90e32e6
Marq W <marquisewest86@duck.com>
2023-04-17 19:32:04 -0500

    updated function to store city locally

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,32 @@
 $(document).ready(function () {
   var APIkey = "e8835723679dd5fa30210dbd83917a83";
 
-  var searchButton = "hello";
+  // var temperature = Math.floor(results.main.temp)
+  // (temp in Kelvin − 273.15) × 9/5 + 32 //formula for converting Kelvin to Farenheit
+
+  var c = 1;
+
+  $("#temp").text("hello");
+  $("#wind").text("hello");
+  $("#humidity").text("hello");
 
   $(".btn").on("click", function (event) {
     event.preventDefault();
     //   console.log("Form submitted");
     var city = $("#chosen-city").val();
+
+    //This query checks the weather
     var queryURL =
       "http://api.openweathermap.org/data/2.5/weather?q=" +
       city +
       "&appid=" +
       APIkey;
 
+      //Store the searched cities in order to display them in the next section
+      localStorage.setItem("City " + c, city);
+      c++;
+
+    // This query checks the forecast
     var queryURL2 =
       "http://api.openweathermap.org/data/2.5/forecast?q=" +
       city +
@@ -21,7 +35,7 @@ $(document).ready(function () {
 
     fetch(queryURL)
       .then((response) => {
-        return response.json(); //ToDo
+        return response.json();
       })
       .then((data) => {
         console.log(data);
