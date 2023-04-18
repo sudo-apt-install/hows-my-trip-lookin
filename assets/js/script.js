20becbd740ae7db22ef7ccc769b2c83d74a3c87f
Marq W <marquisewest86@duck.com>
2023-04-17 23:46:27 -0500

    reworked functions and added recursiveness to be able to search locations again once their buttons are clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,27 @@
 $(document).ready(function () {
   var APIkey = "e8835723679dd5fa30210dbd83917a83";
-
   var todaysDate = dayjs().format('MMMM D, YYYY');
-
   var c = 1; // c is for "counter" (counts to add a number to each searched city in the local storage)
 
-  $(".btn").on("click", function (event) {
-    event.preventDefault();
-    //   console.log("Form submitted");
+  function searchCity() {
     var city = $("#chosen-city").val();
+    var key = "City " + c;
+
+    // Check if the city already exists in the local storage
+    for (var i = 1; i < c; i++) {
+      if (localStorage.getItem("City " + i) === city) {
+        // Update the existing key with the new value
+        key = "City " + i;
+        localStorage.setItem(key, city);
+        break;
+      }
+    }
+
+    // If the city does not exist in the local storage, add a new key-value pair
+    if (!localStorage.getItem(key)) {
+      localStorage.setItem(key, city);
+      c++;
+    }
 
     //This query checks the weather
     var queryURL =
@@ -16,10 +29,6 @@ $(document).ready(function () {
       city +
       "&appid=" +
       APIkey;
-
-      //Store the searched cities in order to display them in the next section
-      localStorage.setItem("City " + c, city);
-      c++;
 
     // This query checks the forecast
     var queryURL2 =
@@ -44,16 +53,22 @@ $(document).ready(function () {
         $("#temp").text("Temp: " + temperature + "°F");
         $("#wind").text("Wind: " + wind + " MPH");
         $("#humidity").text("Humidity: " + humidity + "%");
-        
-        for (var i = 0; i < localStorage.length; i++){
+
+        // Clear previous search buttons
+        $("#previous-searches").empty();
+
+        // Add new search buttons for all cities in the local storage
+        for (var i = 1; i < c; i++) {
           var previousCityButton = document.createElement('button');
           $('#previous-searches').append(previousCityButton);
-          $(previousCityButton).text(localStorage.getItem(localStorage.key(i)));
+          $(previousCityButton).text(localStorage.getItem("City " + i));
           previousCityButton.setAttribute("class", 'prev');
         }
 
+        // Add event listeners to previous search buttons
         $(".prev").on("click", function(event) {
-          alert('Ding!')
+          $("#chosen-city").val($(this).text());
+          searchCity();
         })
 
       });
@@ -65,5 +80,10 @@ $(document).ready(function () {
       .then((data) => {
         console.log(data);
       });
+  }
+
+  $(".btn").on("click", function (event) {
+    event.preventDefault();
+    searchCity();
   });
 });
