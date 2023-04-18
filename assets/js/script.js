09dd88e4fd0a279da1da4b434589a9b5e8826f1f
Marq W <marquisewest86@duck.com>
2023-04-17 20:51:42 -0500

    added weather tracking variables and displayed them accordingly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,7 @@
 $(document).ready(function () {
   var APIkey = "e8835723679dd5fa30210dbd83917a83";
 
-  // var temperature = Math.floor(results.main.temp)
-  // (temp in Kelvin − 273.15) × 9/5 + 32 //formula for converting Kelvin to Farenheit
-
-  var c = 1;
-
-  $("#temp").text("hello");
-  $("#wind").text("hello");
-  $("#humidity").text("hello");
+  var c = 1; // c is for "counter" (counts to add a number to each searched city in the local storage)
 
   $(".btn").on("click", function (event) {
     event.preventDefault();
@@ -39,6 +32,16 @@ $(document).ready(function () {
       })
       .then((data) => {
         console.log(data);
+        var tempInKelvin = (data.main.temp);
+        var temperature = parseFloat((tempInKelvin - 273.15) * 9/5 + 32); //formula for converting Kelvin to Farenheit
+        temperature = temperature.toFixed(2);
+        var humidity = (data.main.humidity);
+        var wind = (data.wind.speed);
+
+        $("#current-city").text(city);
+        $("#temp").text("Temp: " + temperature + "°F");
+        $("#wind").text("Wind: " + wind + " MPH");
+        $("#humidity").text("Humidity:" + humidity + "%"); 
       });
 
     fetch(queryURL2)
