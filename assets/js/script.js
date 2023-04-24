bc15c00e3cc0c61b58e876d1765bdc1054c47b93
Marq W <marquisewest86@duck.com>
2023-04-24 00:31:26 -0500

    fixed issue where previously searched cities wouldn't search again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,21 @@
 $(document).ready(function () {
   var APIkey = "e8835723679dd5fa30210dbd83917a83";
+
   var todaysDate = dayjs().format('MMMM D, YYYY');
+
   var c = 1; // c is for "counter" (counts to add a number to each searched city in the local storage)
 
-  function searchCity() {
-    var city = $("#chosen-city").val();
-    var key = "City " + c;
-
-    // Check if the city already exists in the local storage
-    for (var i = 1; i < c; i++) {
-      if (localStorage.getItem("City " + i) === city) {
-        // Update the existing key with the new value
-        key = "City " + i;
-        localStorage.setItem(key, city);
-        break;
-      }
-    }
-
-    // If the city does not exist in the local storage, add a new key-value pair
-    if (!localStorage.getItem(key)) {
-      localStorage.setItem(key, city);
-      c++;
-    }
-
+  function searchCity(city) {
     //This query checks the weather
     var queryURL =
       "http://api.openweathermap.org/data/2.5/weather?q=" +
       city +
       "&appid=" +
       APIkey;
+
+    //Store the searched cities in order to display them in the next section
+    localStorage.setItem("City " + c, city);
+    c++;
 
     // This query checks the forecast
     var queryURL2 =
@@ -44,7 +31,7 @@ $(document).ready(function () {
       .then((data) => {
         console.log(data);
         var tempInKelvin = (data.main.temp);
-        var temperature = parseFloat((tempInKelvin - 273.15) * 9/5 + 32); //formula for converting Kelvin to Farenheit
+        var temperature = parseFloat((tempInKelvin - 273.15) * 9 / 5 + 32); //formula for converting Kelvin to Farenheit
         temperature = temperature.toFixed(2);
         var humidity = (data.main.humidity);
         var wind = (data.wind.speed);
@@ -54,23 +41,20 @@ $(document).ready(function () {
         $("#wind").text("Wind: " + wind + " MPH");
         $("#humidity").text("Humidity: " + humidity + "%");
 
-        // Clear previous search buttons
-        $("#previous-searches").empty();
+        $('#previous-searches').empty();
 
-        // Add new search buttons for all cities in the local storage
-        for (var i = 1; i < c; i++) {
+        for (var i = 0; i < localStorage.length; i++) {
           var previousCityButton = document.createElement('button');
           $('#previous-searches').append(previousCityButton);
-          $(previousCityButton).text(localStorage.getItem("City " + i));
+          $(previousCityButton).text(localStorage.getItem(localStorage.key(i)));
           previousCityButton.setAttribute("class", 'prev');
+
+          // attach click event listener to the newly created button
+          $(previousCityButton).on("click", function (event) {
+            var previousCity = $(this).text();
+            searchCity(previousCity);
+          });
         }
-
-        // Add event listeners to previous search buttons
-        $(".prev").on("click", function(event) {
-          $("#chosen-city").val($(this).text());
-          searchCity();
-        })
-
       });
 
     fetch(queryURL2)
@@ -82,8 +66,23 @@ $(document).ready(function () {
       });
   }
 
+  function checkLocalStorage(city) {
+    // Check if city is already in local storage
+    for (var i = 0; i < localStorage.length; i++) {
+      if (localStorage.getItem(localStorage.key(i)) === city) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   $(".btn").on("click", function (event) {
     event.preventDefault();
-    searchCity();
+    var city = $("#chosen-city").val();
+    if (checkLocalStorage(city)) {
+      alert("City already searched!");
+    } else {
+      searchCity(city);
+    }
   });
 });
